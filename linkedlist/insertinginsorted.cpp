1ad7fdfacee27b503bffd3b7de2f3410c01baef8
Animesh <animeshdeas@gmail.com>
2023-07-12 09:50:51 +0530

    added

--- linkedlist/insertinginsorted.cpp
+++ linkedlist/insertinginsorted.cpp
@@ -1,14 +1,20 @@
 #include<iostream>
 using namespace std;
 
-struct Node
+class Node
 {
+  public:
   int data;
   struct Node *next;
-}*first = NULL;
+};
 
-void create(int a[],int n) //arraya and size of the function 
+class LinkedList
 {
+    private:
+    Node *first =NULL;
+    public:
+    LinkedList(int a[],int n)
+   {
     struct Node*t,*last;
     first = new Node;
     first->data = a[0];
@@ -23,55 +29,38 @@ void create(int a[],int n) //arraya and size of the function
           last = t;
           
       }
-}
+   } 
 
-//inserting in sorted list function
-
-void insertsorted(struct Node *e,int key)
-{
-    struct Node *q,*t;
-    e = first;
-    q = NULL;
-  
-       t= new Node;
-       t->data = key;  //Basically we are created the node first so that we can just change the nextpart fot pointing the node
-                    //    by t.next = any.next;
-       t->next = NULL;
-   if(first == NULL)
-   {
-    
-       first = t;  //Means it is first node and no other node is present 
-   }
-   else 
+    void insertedsorted(int key)
     {
-       while(e && e->data<key)
-       {
-           q= e;           //If node is present
-           e= e->next;
-       }
-      if(e == first)   //means the value is smaller than first node
-      {
-          t->next = first;
-          first =t;
-      }
-      else
-      {
-          t->next = q->next;
-          q->next = t;     //Value is in between or end of the node;
+        Node *p = first;
+        Node *q = NULL;
+         
+        if(key<first->data)
+        {
+           Node *t = new Node;
+            t->data = key;
+            t->next = first;
+            first = t;
+        } 
+        while(p&&p->data<key)
+        {
+            q= p;
+            p = p->next;
+        }
+
+        Node *temp = new Node;
+        temp->data = key;
+        temp->next = q->next;
+        q->next = temp;
 
 
 
-      }
+    }
 
-
-   
-
-   }
-}
-
-
-void diplay(struct Node *p)
+    void diplay()
 {
+    Node *p  =first;
     while(p!=0)
     {
        cout<<p->data<<" ";
@@ -80,13 +69,18 @@ void diplay(struct Node *p)
     cout<<endl;
 }
 
+    
+};
+
+
+
+
 int main()
 {
     int size = 5;
     int key = 80;
    int arr[size] ={10,20,30,50,70};
-   create(arr,size);
-
-   insertsorted(first,key);
-      diplay(first); 
+    LinkedList l(arr,size);
+    l.insertedsorted(40);
+    l.diplay();
 }
